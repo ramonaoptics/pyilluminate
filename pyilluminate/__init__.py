7e809eb4b07e45973ac58b731e6b4ae92126ff62
Mark Harfouche <mark.harfouche@gmail.com>
2018-05-30 16:37:27 -0700

    John helped me install versioneer

--- pyilluminate/__init__.py
+++ pyilluminate/__init__.py
@@ -8,7 +8,9 @@ Raise exceptions on failure, don't worry about "set" and "get" methods.
 Copyright (C) 2018, Optical Wavefront Laboratories, LLC.
 """
 
-__version__ = "0.2.1"
-
 from .illuminate import Illuminate  # noqa
 from .illuminate import LEDColor  # noqa
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
--- setup.py
+++ setup.py
@@ -4,32 +4,16 @@ from pathlib import Path
 
 from setuptools import find_packages, setup
 
+import versioneer
 
-def get_setup_info(module_path):
-    init_file = Path(module_path) / '__init__.py'
-    with open(init_file, 'r') as f:
-        contents = f.read()
-
-    module = ast.parse(contents)
-    docstring = ast.get_docstring(module)
-
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
-                              contents, re.M)
-    if version_match:
-        return (version_match.group(1), docstring)
-
-    raise RuntimeError("Unable to find version string.")
-
-
-(version, docstring) = get_setup_info('pyilluminate')
 
 setup(name='pyIlluminate',
-      version=version,
-      description=docstring,
+      version=versioneer.get_version(),
       url='https://bitbucket.org/opticalwavefrontlabs/pyilluminate',
       author='Optical Wavefront Laboratories, LLC.',
       author_email='info@opticalwavefrontlabs.com',
       license='BSD',
       install_requires=['pyserial'],
       packages=find_packages(),
+      cmdclass=versioneer.get_cmdclass(),
       )
