bd582b3414ce622107014a433bac42b1a0972fad
ccook21 <cbcook@princeton.edu>
2021-08-03 18:20:49 -0400

    Revert "add color properties for 850 and 940 ir leds"
    This reverts commit 5de8703a85cee04370461a732873b5448eb11d4f.

--- pyilluminate/tests/test_led_color.py
+++ pyilluminate/tests/test_led_color.py
@@ -1,23 +1,15 @@
 from pyilluminate import Illuminate
-from pyilluminate.fake_illuminate import FakeIlluminate
 from time import sleep
 import numpy as np
 import pytest
 
+pytestmark = pytest.mark.device
 
-@pytest.fixture(
-    scope='function',
-    params=[
-        pytest.param(True, marks=pytest.mark.device),
-        False
-    ])
-def light(request):
-    if request.param:
-        with Illuminate() as light:
-            yield light
-    else:
-        with FakeIlluminate() as light:
-            yield light
+
+@pytest.fixture(scope='module')
+def light():
+    with Illuminate() as light:
+        yield light
 
 
 def test_single_color(light):
@@ -46,19 +38,3 @@ def test_green_color_numpy(light):
     light.fill_array()
     sleep(1)
     light.clear()
-
-
-def test_ir_colors(light):
-    light.color = (5, 10, 2)
-    assert light.color_940_ir == 7.5  # average of first two values
-    assert light.color_850_ir == 2
-
-    light.color_940_ir = 20
-    assert light.color == (20, 20, 0)
-    assert light.color_940_ir == 20
-    assert light.color_850_ir == 0
-
-    light.color_850_ir = 10
-    assert light.color == (0, 0, 10)
-    assert light.color_940_ir == 0
-    assert light.color_850_ir == 10
